2a94529ba83b08999e59de5284e912d043dedee2
specture258 <specture258@hanyang.ac.kr>
2024-07-25 04:15:29 +0900

    Revert "게임 시작"
    This reverts commit edf7a8e7ede2d1e3a1ded00698e5572b2297ba6f.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,7 @@
 package baseball;
 
-import baseball.controller.Game;
-
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-
-        Game baseballGame = new Game();
-        baseballGame.play();
     }
 }
