845df5958bf5bc0a7524803342a53d6d94d253be
specture258 <specture258@hanyang.ac.kr>
2024-07-25 03:39:41 +0900

    랜덤 숫자를 반환하는 createRandomNumber 메소드

--- src/test/java/baseball/number/Computer.java
+++ src/test/java/baseball/number/Computer.java
@@ -1,4 +1,23 @@
 package baseball.number;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Computer {
+    public String createRandomNumber() {
+        Set<Integer> set = new LinkedHashSet<>();
+
+        while(true) {
+            if(set.size() == 3) {
+                break;
+            }
+            set.add(pickNumberInRange(1, 9));
+        }
+        return  set.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
 }
