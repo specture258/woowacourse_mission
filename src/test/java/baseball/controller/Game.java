cab5dc686ec1f80977ec11bc64de672abd5cde35
specture258 <specture258@hanyang.ac.kr>
2024-07-25 03:43:39 +0900

    게임을 진행하는 play 메소드

--- src/test/java/baseball/controller/Game.java
+++ src/test/java/baseball/controller/Game.java
@@ -1,6 +1,20 @@
 package baseball.controller;
 
+import baseball.number.Computer;
+
 public class Game {
+
+    Ball ball = new Ball();
+    Strike strike = new Strike();
+    private boolean exit = false;
+
+    public void play(){
+
+        Computer computer = new Computer();
+        String computerInputNumber = computer.createRandomNumber();
+        while(!exit){
+            flow(ball, strike, computerInputNumber );
+        }
 
     public String[] StrToStrArr(String str){
         return str.split("");
