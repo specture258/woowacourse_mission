bff55234bda198fa926a3d4f432f042eaec6cea5
specture258 <specture258@hanyang.ac.kr>
2024-07-25 03:46:55 +0900

    입력을 받아서 볼/스트라이크/낫싱을 판정하는 flow 메소드

--- src/test/java/baseball/controller/Game.java
+++ src/test/java/baseball/controller/Game.java
@@ -1,6 +1,7 @@
 package baseball.controller;
 
 import baseball.number.Computer;
+import baseball.number.User;
 
 public class Game {
 
@@ -15,8 +16,36 @@ public class Game {
         while(!exit){
             flow(ball, strike, computerInputNumber );
         }
+    }
+
+
+    public void flow(Ball ball, Strike strike, String computerInputNumber){
+        User user = new User();
+        String userInputNumber = user.getInputNumber();
+        strike.setCnt(0);
+
+        boolean ballCorrect = ball.isCorrect(StrToStrArr(userInputNumber), StrToStrArr(computerInputNumber));
+        boolean strikeCorrect = strike.isCorrect(StrToStrArr(userInputNumber), StrToStrArr(computerInputNumber));
+
+        if(strike.getCnt()== 3){
+            System.out.print("3개의 숫자를 모두 맞히셨습니다! ");
+            System.out.println(Message.gameMessage());
+            chooseOption();
+        }
+        if(!ballCorrect && !strikeCorrect) {
+            System.out.println(Message.nothingMessage());
+        }
+
+        if(ballCorrect && !strikeCorrect) {
+            System.out.println();
+        }
+    }
+
+
 
     public String[] StrToStrArr(String str){
         return str.split("");
     }
+
+
 }
