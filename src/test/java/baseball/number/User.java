74cb507bfe768c02826f4f8603560b4c9294ffeb
specture258 <specture258@hanyang.ac.kr>
2024-07-25 03:39:04 +0900

    입력받은 숫자를 반환하는 getInputNuber 메소드

--- src/test/java/baseball/number/User.java
+++ src/test/java/baseball/number/User.java
@@ -1,4 +1,19 @@
 package baseball.number;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class User {
+    String inputNumber;
+
+    public String getInputNumber(){
+
+        System.out.print("숫자를 입력해주세요 : ");
+        inputNumber = readLine();
+
+        if (inputNumber.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        return inputNumber;
+
+    }
 }
