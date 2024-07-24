3fff490dc25568ccd9f119e602f267a787f55e53
specture258 <specture258@hanyang.ac.kr>
2024-07-25 03:55:27 +0900

    스트라이크를 판정하는 isCorrect 메소드

---
+++ src/test/java/baseball/validation/Strike.java
@@ -0,0 +1,36 @@
+package baseball.validation;
+
+public class Strike implements Correctable{
+    int cnt =0;
+    @Override
+    public boolean isCorrect(String[] input, String[] answer) {
+        int pointer =0;
+        for (String s : input) {
+            if (s.equals(answer[pointer])) {
+                cnt++;
+            }
+            pointer++;
+        }
+
+        if(cnt == 3){
+            System.out.println(Message.threeStrike());
+            return true;
+        }else if(cnt == 2){
+            System.out.println(Message.twoStrike());
+            return true;
+        }else if(cnt == 1){
+            System.out.println(Message.oneStrike());
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public int getCnt() {
+        return cnt;
+    }
+
+    public void setCnt(int cnt) {
+        this.cnt = cnt;
+    }
+}
