22b72195de3593142e88c985d7b57c1a5a60a901
specture258 <specture258@hanyang.ac.kr>
2024-08-04 22:10:38 +0900

    패키지 구조 변경 (controller/domain/view)

--- src/main/java/baseball/number/User.java
+++ src/main/java/baseball/number/User.java
@@ -3,12 +3,11 @@ package baseball.number;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
-    private String inputNumber;
 
     public String getInputNumber(){
 
         System.out.print("숫자를 입력해주세요 : ");
-        inputNumber = readLine();
+        String inputNumber = readLine();
 
         if (inputNumber.length() != 3) {
             throw new IllegalArgumentException();
