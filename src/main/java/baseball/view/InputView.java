3b5a2a278818672e76f60c06686abe027d06af16
specture258 <specture258@hanyang.ac.kr>
2024-08-08 09:58:32 +0900

    입력만 따로 관리하는 InputView 클래스

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,8 +3,25 @@ package baseball.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class InputView {
-    public String InputNumber(){
-        String inputNumber = readLine();
-        return inputNumber;
+
+    private final InputViewValidation inputViewValidation;
+
+    public InputView(InputViewValidation inputViewValidation) {
+        this.inputViewValidation = inputViewValidation;
     }
+
+    public String InputUserNumber(){
+        String input =  readLine();
+        inputViewValidation.validateLength(input);
+        inputViewValidation.validateNumber(input);
+        inputViewValidation.validateDuplicate(input);
+        return input;
+    }
+
+    public String InputCommand(){
+         String input = readLine();
+         inputViewValidation.validateCommand(input);
+         return input;
+    }
+
 }
