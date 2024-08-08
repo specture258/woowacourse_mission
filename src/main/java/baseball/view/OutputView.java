f0d01f2f09353aa18b3e79187ced59e9f51d244f
specture258 <specture258@hanyang.ac.kr>
2024-08-08 09:58:09 +0900

    출력만 따로 관리하는 OutputView 클래스

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,41 @@
 package baseball.view;
 
 public class OutputView {
+
+    public void inputNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+
+    public String ballMessage(int[] cnt){
+        if(cnt[0]==0){
+            return "";
+        }
+        return cnt[0] + "볼 ";
+    }
+
+    public String strikeMessage(int[] cnt){
+        if(cnt[1]==0){
+            return "";
+        }
+        return cnt[1] + "스트라이크 ";
+    }
+
+     public String nothingMessage(){
+        return "낫싱";
+    }
+
+    public String allCorrectMessage(){
+        return "3개의 숫자를 모두 맞히셨습니다!";
+    }
+
+    public String endMessage(){
+        return "게임 종료 ";
+    }
+
+    public String restartMessage(){
+        return "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    }
+
+
 }
